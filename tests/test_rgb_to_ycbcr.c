7cf6563fcb9e5a423f583d5d5e0590f89fea279e
Paul Bottner <paul.bottner@grenoble-inp.org>
2026-04-28 14:40:26 +0200

    ajout des tests pour la fonction rgb_to_ycbcr

---
+++ tests/test_rgb_to_ycbcr.c
@@ -0,0 +1,85 @@
+#include "unity.h"
+#include "rgb_to_ycbcr.h"
+
+void setUp(void) {
+    // set stuff up here
+}
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+void test_valeurs_min_y(void) {
+
+    struct couleur_rgb_t couleur = {0,0,0};
+    struct couleur_ycbcr_t ycbcr = rgb_to_ycbcr(couleur);
+
+    TEST_ASSERT_EQUAL_UINT8(0, ycbcr.y);
+    TEST_ASSERT_EQUAL_UINT8(128, ycbcr.cb);
+    TEST_ASSERT_EQUAL_UINT8(128, ycbcr.cr);
+}
+
+void test_valeurs_max_y(void) {
+
+    struct couleur_rgb_t couleur = {255,255,255};
+    struct couleur_ycbcr_t ycbcr = rgb_to_ycbcr(couleur);
+
+    TEST_ASSERT_EQUAL_UINT8(255, ycbcr.y);
+    TEST_ASSERT_EQUAL_UINT8(128, ycbcr.cb);
+    TEST_ASSERT_EQUAL_UINT8(128, ycbcr.cr);
+}
+
+void test_valeurs_min_cb(void) {
+
+    struct couleur_rgb_t couleur = {255,255,0};
+    struct couleur_ycbcr_t ycbcr = rgb_to_ycbcr(couleur);
+
+    TEST_ASSERT_UINT8_WITHIN(1, 226, ycbcr.y);
+    TEST_ASSERT_EQUAL_UINT8(0, ycbcr.cb);
+    TEST_ASSERT_UINT8_WITHIN(1, 149, ycbcr.cr);
+}
+
+void test_valeurs_max_cb(void) {
+
+    struct couleur_rgb_t couleur = {0,0,255};
+    struct couleur_ycbcr_t ycbcr = rgb_to_ycbcr(couleur);
+
+    TEST_ASSERT_UINT8_WITHIN(1, 29, ycbcr.y);
+    TEST_ASSERT_EQUAL_UINT8(255, ycbcr.cb);
+    TEST_ASSERT_UINT8_WITHIN(1, 107, ycbcr.cr);
+}
+
+void test_valeurs_min_cr(void) {
+
+    struct couleur_rgb_t couleur = {0,255,255};
+    struct couleur_ycbcr_t ycbcr = rgb_to_ycbcr(couleur);
+
+    TEST_ASSERT_UINT8_WITHIN(1, 179, ycbcr.y);
+    TEST_ASSERT_UINT8_WITHIN(1, 171, ycbcr.cb);
+    TEST_ASSERT_EQUAL_UINT8(0, ycbcr.cr);
+}
+
+void test_valeurs_max_cr(void) {
+
+    struct couleur_rgb_t couleur = {255,0,0};
+    struct couleur_ycbcr_t ycbcr = rgb_to_ycbcr(couleur);
+
+    TEST_ASSERT_UINT8_WITHIN(1, 76, ycbcr.y);
+    TEST_ASSERT_UINT8_WITHIN(1, 85, ycbcr.cb);
+    TEST_ASSERT_EQUAL_UINT8(255, ycbcr.cr);
+}
+
+
+int main(void) {
+
+    UNITY_BEGIN();
+
+    RUN_TEST(test_valeurs_min_y);
+    RUN_TEST(test_valeurs_max_y);
+    RUN_TEST(test_valeurs_min_cb);
+    RUN_TEST(test_valeurs_max_cb);
+    RUN_TEST(test_valeurs_min_cr);
+    RUN_TEST(test_valeurs_max_cr);
+    
+    return UNITY_END();
+}
