3f8f96cb67ae32500581292766c6ebfe5dd55dda
Khalil <mohamed-khalil.becharai@grenoble-inp.org>
2026-05-04 15:31:03 +0200

    encodage magnitude

---
+++ include/magnitude.h
@@ -0,0 +1,20 @@
+#ifndef _MAGNITUDE_H_
+#define _MAGNITUDE_H_
+#include <stdint.h>
+
+
+typedef struct Magnitude
+{
+   uint16_t indice;
+   uint8_t class_mag ;
+
+} Magnitude;
+
+
+void codage_magnitude(int16_t bloc[64],int16_t * dc_prec,Magnitude bloc_enc[64]);
+
+Magnitude encoder_val(int16_t val);
+
+
+
+#endif 
---
+++ src/magnitude.c
@@ -0,0 +1,54 @@
+#include <magnitude.h>
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void codage_magnitude(int16_t bloc[64],int16_t * dc_prec,Magnitude bloc_enc[64]){
+
+    bloc_enc[0]=encoder_val(bloc[0]- (*dc_prec));
+    int16_t ac_prec=0;
+    for (int i = 1; i < 64; i++)
+    {
+       bloc_enc[i]=encoder_val(bloc[i]);
+        
+    }
+    *dc_prec=bloc[0];
+
+}
+
+Magnitude encoder_val(int16_t val){
+    int16_t tmp=abs(val);
+    uint8_t magnitude=0;
+    uint16_t puissance_2=1;
+    Magnitude val_enc;
+    
+    if (val==0)
+    {
+        return (Magnitude){0,0};
+    }
+    
+
+    while ( tmp > 0)
+    { 
+      magnitude++;
+      puissance_2*=2;
+      tmp=tmp >> 1;
+    }
+    val_enc.class_mag=magnitude;
+    uint16_t indice;
+    int16_t inf_int=puissance_2/2;
+    int16_t sup_int=puissance_2-1;
+    
+    if (val<0)
+    {
+       indice=sup_int+val;
+    }
+    else
+    {
+     indice=val;
+    }
+    val_enc.indice= indice;
+
+    return val_enc;
+
+}
