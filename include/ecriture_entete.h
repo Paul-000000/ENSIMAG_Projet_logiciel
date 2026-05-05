2cef26ec464b6b290cdd314dd20b0190ace938c8
Khalil <mohamed-khalil.becharai@grenoble-inp.org>
2026-05-05 14:06:21 +0200

    ecrtiture de l'entete non encore termine

---
+++ include/ecriture_entete.h
@@ -0,0 +1,16 @@
+#ifndef _ECRITURE_ENTETE_H_
+#define _ECRITURE_ENTETE_H_
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+
+#define Marqueur_APP0 0xffe0
+
+
+bool ecrire_appx(FILE *f);
+
+
+
+#endif /* _ECRITURE_ENTETE_H_ */
