3fb8fa51298bd755f89dfe9dc1013808db61fe10
Paul Bottner <paul.bottner@grenoble-inp.org>
2026-05-07 11:01:21 +0200

    amélioration du graphique des structures de données

--- include/dct.h
+++ include/dct.h
@@ -6,7 +6,7 @@
 
 
 // crée une table de cosinus pour ne pas les recalculer par la suite
-void init_table_cosinus();
+//void init_table_cosinus();
 
 // transforme la matrice bloc_spatial en matrice sous forme fréquentielle
 void applique_dct(const uint8_t bloc_spatial[64], int16_t bloc_frequentiel[64]);
