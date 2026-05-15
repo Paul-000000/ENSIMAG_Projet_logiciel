a0deac49f48dfdb306775285a84ffb1176b50fb8
Khalil <mohamed-khalil.becharai@grenoble-inp.org>
2026-05-15 10:22:55 +0200

    calcule de temps d'execution dans le main commente

--- src/main.c
+++ src/main.c
@@ -16,8 +16,8 @@
 
 int main(int argc, char **argv) {
     
-    double t_dct = 0, t_lecture =0, t_ecriture = 0, t_decoupage = 0, t_zigzag_quantification = 0, t_magnitude = 0, t_rle = 0, t_huffman = 0; // a retirer
-    clock_t debut = clock(); // a retirer
+    // double t_dct = 0, t_lecture =0, t_ecriture = 0, t_decoupage = 0, t_zigzag_quantification = 0, t_magnitude = 0, t_rle = 0, t_huffman = 0; // a retirer
+    // clock_t debut = clock(); // a retirer
 
 
     // commande
@@ -78,44 +78,44 @@ int main(int argc, char **argv) {
 
         while (true) {
 
-            clock_t d = clock(); // a retirer
+            // clock_t d = clock(); // a retirer
             reste_mcu = mcu_couleur_suivant(&iterateur, mcu_couleur);
             if (!reste_mcu) {
                 break;
             }
-            t_lecture += (clock() - d); // a retirer
+            // t_lecture += (clock() - d); // a retirer
 
-            d = clock(); // a retirer
+            // d = clock(); // a retirer
             decouper_matrices_ycbcr(mcu_couleur, iterateur.largeur_mcu, iterateur.hauteur_mcu, dim_cbcr, &vecteurs);
-            t_decoupage += (clock() - d); // a retirer
+            // t_decoupage += (clock() - d); // a retirer
 
             for (uint8_t i = 0; i < vecteurs.nb_vecteurs; i++) {
 
                 vecteur = vecteurs.vecteurs[i];
                 
-                d = clock(); // a retirer
+                // d = clock(); // a retirer
                 applique_dct(vecteur.valeur);
-                t_dct += (clock() - d); // a retirer
+                // t_dct += (clock() - d); // a retirer
 
-                d = clock(); // a retirer
+                // d = clock(); // a retirer
                 applique_zigzag_quantification(vecteur.valeur, vecteur.composante, vecteur_entiers);
-                t_zigzag_quantification += (clock() - d); // a retirer
+                // t_zigzag_quantification += (clock() - d); // a retirer
 
-                d = clock(); // a retirer
+                // d = clock(); // a retirer
                 codage_magnitude(vecteur_entiers, &(dc_prec_y_cb_cr[vecteur.composante]), bloc_enc);
-                t_magnitude += (clock() - d); // a retirer
+                // t_magnitude += (clock() - d); // a retirer
 
-                d = clock(); // a retirer
+                // d = clock(); // a retirer
                 rle(vecteur_entiers, &symboles_rle_ac, bloc_enc);
-                t_rle += (clock() - d); // a retirer
+                // t_rle += (clock() - d); // a retirer
 
-                d = clock(); // a retirer
+                // d = clock(); // a retirer
                 encoder_coefficients_huffman(bloc_enc, &symboles_rle_ac, vecteur.composante, &ac_dc);
-                t_huffman += (clock() - d); // a retirer
+                // t_huffman += (clock() - d); // a retirer
 
-                d = clock(); // a retirer
+                // d = clock(); // a retirer
                 ajouter_donnees_compressees(&ac_dc, &flux);
-                t_ecriture += (clock() - d); // a retirer
+                // t_ecriture += (clock() - d); // a retirer
             }   
         }
 
@@ -153,15 +153,15 @@ int main(int argc, char **argv) {
     liberer_parametres_commande(&parametres);
 
 
-    printf("effectué en : %.3fs\n", (double)(clock() - debut) / CLOCKS_PER_SEC); // a retirer
-    printf("lecture : %.3fs\n", t_lecture / CLOCKS_PER_SEC); // a retirer
-    printf("decoupage : %.3fs\n", t_decoupage / CLOCKS_PER_SEC); // a retirer
-    printf("dct : %.3fs\n", t_dct / CLOCKS_PER_SEC); // a retirer
-    printf("zigzag et quantification: %.3fs\n", t_zigzag_quantification / CLOCKS_PER_SEC); // a retirer
-    printf("magnitude : %.3fs\n", t_magnitude / CLOCKS_PER_SEC); // a retirer
-    printf("rle : %.3fs\n", t_rle / CLOCKS_PER_SEC); // a retirer
-    printf("huffman : %.3fs\n", t_huffman / CLOCKS_PER_SEC); // a retirer
-    printf("ecriture : %.3fs\n", t_ecriture / CLOCKS_PER_SEC); // a retirer
+    // printf("effectué en : %.3fs\n", (double)(clock() - debut) / CLOCKS_PER_SEC); // a retirer
+    // printf("lecture : %.3fs\n", t_lecture / CLOCKS_PER_SEC); // a retirer
+    // printf("decoupage : %.3fs\n", t_decoupage / CLOCKS_PER_SEC); // a retirer
+    // printf("dct : %.3fs\n", t_dct / CLOCKS_PER_SEC); // a retirer
+    // printf("zigzag et quantification: %.3fs\n", t_zigzag_quantification / CLOCKS_PER_SEC); // a retirer
+    // printf("magnitude : %.3fs\n", t_magnitude / CLOCKS_PER_SEC); // a retirer
+    // printf("rle : %.3fs\n", t_rle / CLOCKS_PER_SEC); // a retirer
+    // printf("huffman : %.3fs\n", t_huffman / CLOCKS_PER_SEC); // a retirer
+    // printf("ecriture : %.3fs\n", t_ecriture / CLOCKS_PER_SEC); // a retirer
 
 
     return EXIT_SUCCESS;
